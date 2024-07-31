f9b23618cd14f768024ca52c3f1e3459ee6fc3c5
Ty Mackall <tymackall10@gmail.com>
2024-07-31 10:21:08 +0000

    configured everything

--- s3-python/python-scripts/list-buckets.py
+++ s3-python/python-scripts/list-buckets.py
@@ -1,11 +1,22 @@
+#FOR ME - to execute code from terminal give all the files in the folder permission to be executed
+#using chmod u+x folder/sub-folder/* call python file by python then path file
+
+#!usr/bin/env python
+
+
+#import boto
+import boto3
+
 # initialize a session using s3
 s3 = boto3.client('s3')
 
 # get list of buckets
-s3.list_buckets()
+response = s3.list_buckets()
+
+##print(type(response))
 
 # extract bucket names and creation date
-buckets = response('Buckets')
+buckets = response['Buckets']
 
 # Sort buckets by creation date, most recent first
 sorted_buckets = sorted(buckets, key=lambda b: b['CreationDate'], reverse=True)
