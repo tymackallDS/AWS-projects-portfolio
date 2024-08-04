b55301b161959a233aa229f6e8cf71beb5fae485
Ty Mackall <tymackall10@gmail.com>
2024-08-04 04:04:50 +0000

    terraform

---
+++ s3/iac/terraform/main.tf
@@ -0,0 +1,14 @@
+#provides api access to the cloud service provider
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.61.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
