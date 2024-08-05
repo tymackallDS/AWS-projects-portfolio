7fa0ae1d47d5024500fd3a7491e448751096d378
Ty Mackall <tymackall10@gmail.com>
2024-08-05 00:36:47 +0000

    etags

---
+++ s3/etags/main.tf
@@ -0,0 +1,10 @@
+https://registry.terraform.io/providers/hashicorp/aws/latest provider
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.61.0"
+    }
+  }
+}
